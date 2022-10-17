167617ffd3f9df8a826d9b539a9aa1654adafa3f
srivalli <snc.srivalli1@gmail.com>
2022-10-17 09:09:48 +0530

    17/10/2022 commit

--- src/test/java/javascriptExecutor/InstagramDisabledElementClick.java
+++ src/test/java/javascriptExecutor/InstagramDisabledElementClick.java
@@ -12,7 +12,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class InstagramDisabledElementClick {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -20,15 +20,21 @@ public class InstagramDisabledElementClick {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 		WebElement userNameTextField = driver.findElement(By.name("username"));
-		userNameTextField.clear();
 		WebElement passwordTextField = driver.findElement(By.name("password"));
-		passwordTextField.clear();
 		WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
 		
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
-		jse.executeScript("arguments[0].value=arguments[1];", userNameTextField,"9177432389");
-		jse.executeScript("arguments[0].value=arguments[1];", passwordTextField,"135689");
 		
+		System.out.println("Before entering username and password");
+		if(loginButton.isEnabled())
+			System.out.println("Enabled");
+		else
+			System.out.println("Disabled");
+		
+		userNameTextField.sendKeys("9177432389");
+		passwordTextField.sendKeys("shitoku@87");
+		
+		System.out.println("After entering username and password");
 		if(loginButton.isEnabled())
 			System.out.println("Enabled");
 		else
